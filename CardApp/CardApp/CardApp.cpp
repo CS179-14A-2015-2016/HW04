e7e61d0ffed27714aca568375f133ef8b03f7fe3
JNiccoMatias <jenaromatias@yahoo.com>
2015-09-08 17:57:38 +0800

    VB Project Folder Start
    Just the beginning

---
+++ CardApp/CardApp/CardApp.cpp
@@ -0,0 +1,40 @@
+// CardApp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+bool gameRunning;
+bool playerTurn;
+int player1Hand[15];
+int player2Hand[15];
+int player1ChoiceCard;
+int player2ChoiceCard;
+int player1Score;
+int player2Score;
+
+int CompareCard(int cardA, int cardB)
+{
+	//return 0 if draw, 1 if P1 win , 2 if P2 win
+	if ((cardA != 0) && (cardB != 0))
+	{
+
+	}
+	else if (cardA == 0)
+	{
+
+	}
+	else if (cardB == 0)
+	{
+
+	}
+	else
+	{
+
+	}
+}
+
+int main()
+{
+    return 0;
+}
+
