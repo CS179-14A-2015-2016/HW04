6e00018f1407d646f4fb207e75af7d7aa687b26e
JNiccoMatias <jenaromatias@yahoo.com>
2015-09-10 06:46:07 +0800

    Update
    Not yet done, will keep working once I arrive at campus.

--- CardGame/CardGame.cpp
+++ CardGame/CardGame.cpp
@@ -2,14 +2,17 @@
 //
 
 #include <iostream>
+#include <random>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 bool gameRunning;
 bool playerTurn;
-int player1Hand[15];
-int player2Hand[15];
 int player1ChoiceCard;
 int player2ChoiceCard;
+vector<int> player1Hand;
+vector<int> player2Hand;
 int player1Score;
 int player2Score;
 int screenClear;
@@ -22,7 +25,7 @@ int EndGameCounterDEBUG;
 int CompareCard()
 {
     //return 0 if draw, 1 if P1 win , 2 if P2 win
-    
+
     if ((cardA != 1) && (cardB != 13))
     {
         cout << "Player A wins!" << endl;
@@ -108,31 +111,38 @@ int CompareCard()
 int Game()
 {
     //cout << "hello world \n";
-    
-    
-    
-    cout << "Press 'Enter' when Player 1 is ready... \n";
-    
-    if (cin.get() == '\n')
+
+    cout << "Press 'Enter' when Player A is ready... \n";
+
+    if (cin.get() == ' \n ')
     {
         //int tempCount;
         while (screenClear <= 30)
         {
-            cout << "\n";
+            cout << "*\n";
             screenClear++;
         }
         screenClear = 0;
     }
-    
-    
-    
-    EndGameCounterDEBUG++;
-    
-    if (EndGameCounterDEBUG >= 15)
+
+    cout << "Available Cards: ";
+    for (auto i = player1Hand.begin(); i != player1Hand.end(); ++i)
     {
-        gameRunning = 0;
+        cout << *i << " ";
     }
-    
+    cout << "\n" << "a>";
+
+
+
+
+
+    //EndGameCounterDEBUG++;
+
+    //if (EndGameCounterDEBUG >= 15)
+    //{
+        //gameRunning = 0;
+    //}
+
     return 0;
 }
 
@@ -142,10 +152,10 @@ int main()
     cout << "1. How to Play" <<endl;
     cout << "2. Start Game" <<endl;
     cout << "3. Exit" <<endl;
-    
+
     do {cin>>x;}
     while (x == 3);
-    
+
     if (x == 1)
     {
         cout << "How to Play" <<endl;
@@ -153,30 +163,36 @@ int main()
         cout << "2.  Each player uses a card to joust. Except for the jokers, a higher card beats a lower card. But 1 beats 13. The jokers randomly wins or loses against the other card. If both players chose jokers, then there's 50% chance of a draw, 25% chance of the the first player winning, and 25% chance of the other player winning." <<endl;
         cout << "3.  A player gets a point only when he wins the joust. For a draw, no points will be given to any player." <<endl;
         cout << "4.  The jousts will keep on going until all cards are used up. So there will be 15 rounds/jousts in total." << endl;
-        
+
         do
         {
             cin >> x;
         }
         while (x == 3);
     }
-    
+
     if (x == 2)
     {
         gameRunning = 1;
-        
+        player1Hand = {0,0,1,2,3,4,5,6,7,8,9,10,11,12,13};
+        player2Hand = {0,0,1,2,3,4,5,6,7,8,9,10,11,12,13};
+
         while (gameRunning == 1)
         {
             Game();
+
+            if ((player1Hand.empty()) && (player2Hand.empty()))
+            {
+                gameRunning = 0;
+            }
         }
-        
+
         cout << "end \n";
-        
+
         cin.get();
-        return 0;
-        
+        //return 0;
     }
-    
+
     if (x == 3)
     {
         do
@@ -184,13 +200,13 @@ int main()
             cin >> x;
         }
         while (x == 3);
-        
+
         system("pause");
     }
-    
-    
-    
+
+
+
     return 0;
-    
+
 }
 
