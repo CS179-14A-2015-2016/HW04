468edcd75140d8a3209ef7df277f738906a26321
JNiccoMatias <jenaromatias@yahoo.com>
2015-09-09 18:50:17 +0800

    Updated Game
    Incomplete game function.  Still Working.

--- CardGame2/CardGame2/CardGame2.cpp
+++ CardGame2/CardGame2/CardGame2.cpp
@@ -2,6 +2,8 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
 bool gameRunning;
 bool playerTurn;
@@ -11,6 +13,9 @@ int player1ChoiceCard;
 int player2ChoiceCard;
 int player1Score;
 int player2Score;
+int screenClear;
+
+int EndGameCounterDEBUG;
 
 int CompareCard(int cardA, int cardB)
 {
@@ -31,10 +36,53 @@ int CompareCard(int cardA, int cardB)
 	{
 
 	}
+
+	return 0;
+}
+
+int Game()
+{
+	//cout << "hello world \n";
+
+	
+
+	cout << "Press 'Enter' when Player 1 is ready... \n";
+
+	if (cin.get() == '\n')
+	{
+		//int tempCount;
+		while (screenClear <= 30)
+		{
+			cout << "\n";
+			screenClear++;
+		}
+		screenClear = 0;
+	}
+
+
+
+	EndGameCounterDEBUG++;
+
+	if (EndGameCounterDEBUG >= 15)
+	{
+		gameRunning = 0;
+	}
+
+	return 0;
 }
 
 int main()
 {
+	gameRunning = 1;
+
+	while (gameRunning == 1)
+	{
+		Game();
+	}
+	
+	cout << "end \n";
+
+	cin.get();
     return 0;
 }
 
